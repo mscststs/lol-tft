214848d2ae56270c0e3ee0565d3e23d351bebf22
mosaicpan <mosaicpan@tencent.com>
2022-12-05 15:34:25 +0800

    fix:something

--- src/lol/components/header.vue
+++ src/lol/components/header.vue
@@ -4,52 +4,47 @@
     <div class="block">
       <select v-model="selectArea" class="selector">
         <option value="" disabled class="selector-options">选择区服</option>
-        <option v-for="item of areas"
-        class="selector-options"
-        :key="item.v"
-        :value="item.v">
-          {{item.t}}
+        <option
+          v-for="item of areas"
+          class="selector-options"
+          :key="item.v"
+          :value="item.v"
+        >
+          {{ item.t }}
         </option>
       </select>
     </div>
     <div class="gutter"></div>
-    <div class="block button" @click="toSelf" v-if="$route.params.area">
-      我的战绩
-    </div>
+    <div class="block button" @click="toSelf" v-if="$route.params.area">我的战绩</div>
     <div class="gutter"></div>
-    <div class="block button" @click="logout">
-      注销
-    </div>
+    <div class="block button" @click="logout">注销</div>
     <div class="gutter"></div>
     <div class="space"></div>
 
     <!-- 召唤师搜索 -->
     <div class="block">
-      <input class="searchInput" type="text" v-model="searchText"
+      <input
+        class="searchInput"
+        type="text"
+        v-model="searchText"
         placeholder="搜索召唤师"
         @keypress.enter="search"
-      >
+      />
     </div>
 
     <div class="space"></div>
 
     <div class="gutter"></div>
 
-    <div class="block button" @click="donate">
-      向开发者捐赠
-    </div>
+    <div class="block button" @click="donate">向开发者捐赠</div>
 
     <div class="gutter"></div>
 
-    <div class="block button" @click="mainPage">
-      主页
-    </div>
+    <div class="block button" @click="mainPage">主页</div>
 
     <div class="gutter"></div>
 
-    <div class="block button" @click="openRepo">
-      Github
-    </div>
+    <div class="block button" @click="openRepo">Github</div>
 
     <!-- <div class="gutter"></div> -->
 
@@ -60,119 +55,119 @@
 </template>
 
 <script>
-import areas from "../const/areas"
-import storage from "../utils/storage"
+import areas from "../const/areas";
+import storage from "../utils/storage";
 
-import {sleep} from "../utils/index"
+import { sleep } from "../utils/index";
 
 export default {
-  data(){
+  data() {
     return {
       areas,
       selectArea: "",
-      searchText:"",
-    }
+      searchText: "",
+    };
   },
-  mounted(){
-    if(this.$route.params.area){
-      this.selectArea = this.$route.params.area
-    }else if(storage.get("area")){
+  mounted() {
+    if (this.$route.params.area) {
+      this.selectArea = this.$route.params.area;
+    } else if (storage.get("area")) {
       this.selectArea = storage.get("area");
     }
   },
-  watch:{
-    selectArea(val){
-      if(val){
-        storage.set("area",val);
-        if(this.$route.params.area !== val){
-          this.$router.replace({ name: 'area', params: { area: val }})
-
+  watch: {
+    selectArea(val) {
+      if (val) {
+        storage.set("area", val);
+        if (this.$route.params.area !== val) {
+          this.$router.replace({ name: "area", params: { area: val } });
         }
       }
-    }
+    },
   },
-  methods:{
-    search(){
-      if(this.searchText){
-        this.$router.push({ name: 'search', params: { nickname: this.searchText }});
+  methods: {
+    search() {
+      if (this.searchText) {
+        this.$router.push({ name: "search", params: { nickname: this.searchText } });
       }
     },
-    toSelf(){
-      this.$router.replace({ name: 'area', params: { area: this.$route.params.area }});
+    toSelf() {
+      this.$router.replace({ name: "area", params: { area: this.selectArea } });
     },
-    async logout(){
+    async logout() {
       WegameLogin.default.logout();
       await sleep(500);
       location.reload();
     },
-    donate(){
-      window.open("https://wsd.pages.interface.work/view?src=https%3A%2F%2Fpic.mscststs.com%2Fdonate.jpg");
+    donate() {
+      window.open(
+        "https://wsd.pages.interface.work/view?src=https%3A%2F%2Fpic.mscststs.com%2Fdonate.jpg"
+      );
     },
-    mainPage(){
+    mainPage() {
       window.open("https://greasyfork.org/zh-CN/scripts/454184");
     },
-    openRepo(){
+    openRepo() {
       window.open("https://github.com/mscststs/lol-tft");
     },
-    bugReport(){
-      window.open("https://github.com/mscststs/lol-tft/issues/new")
-    }
-  }
-}
+    bugReport() {
+      window.open("https://github.com/mscststs/lol-tft/issues/new");
+    },
+  },
+};
 </script>
 
 <style lang="less">
-  .tftHeader{
-    height:75px;
-    width:100%;
-    display:flex;
-    flex:none;
-    background-color: #1a2026;
-    border-bottom: 1px solid #24292e;
-    
-    .selector{
-      background-color: transparent;
-      border:none;
-      outline:none;
-      color:#b59758;
-      .selector-options{
-        background-color:#000;
-        color:#b59758;
-        padding: 3px 0;
+.tftHeader {
+  height: 75px;
+  width: 100%;
+  display: flex;
+  flex: none;
+  background-color: #1a2026;
+  border-bottom: 1px solid #24292e;
 
-      }
-    }
-    .block{
-      display:flex;
-      flex:none;
-      justify-content: center;
-      align-items: center;
-      padding:20px;
-      font-size:15px;
-      &.button{
-        cursor:pointer;
-        color:#b59758;
-        font-size:14px;
-        &:hover{
-          background-color:rgba(255,255,255,0.05);
-        }
-      }
-      .searchInput{
-        background-color: rgba(0,0,0,0.5);
-        color:#ddd;
-        border-radius: 20px;
-        height:20px;
-        border:1px solid #544729;
-        outline:none;
-        padding: 5px 10px;
-      }
-    }
-    .gutter{
-      width:1px;
-      background-color: #24292e;
-    }
-    .space{
-      flex:auto;
+  .selector {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    color: #b59758;
+    .selector-options {
+      background-color: #000;
+      color: #b59758;
+      padding: 3px 0;
     }
   }
+  .block {
+    display: flex;
+    flex: none;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    font-size: 15px;
+    &.button {
+      cursor: pointer;
+      color: #b59758;
+      font-size: 14px;
+      &:hover {
+        background-color: rgba(255, 255, 255, 0.05);
+      }
+    }
+    .searchInput {
+      background-color: rgba(0, 0, 0, 0.5);
+      color: #ddd;
+      border-radius: 20px;
+      height: 20px;
+      border: 1px solid #544729;
+      outline: none;
+      padding: 5px 10px;
+    }
+  }
+  .gutter {
+    width: 1px;
+    background-color: #24292e;
+  }
+  .space {
+    flex: auto;
+  }
+}
 </style>
--- src/lol/view/search.vue
+++ src/lol/view/search.vue
@@ -2,11 +2,12 @@
   <div class="common" v-if="ready">
     <panel title="搜索结果">
       <div class="errorText" v-if="errorText">
-        {{errorText}}
+        {{ errorText }}
       </div>
 
       <div class="players" v-if="players.length">
-        <div class="player"
+        <div
+          class="player"
           v-for="player of players"
           :key="player.openid"
           @click="toPlayer(player)"
@@ -14,126 +15,124 @@
           <icons class="icon" type="usericon" :id="player.icon_id"></icons>
           <div class="gutter"></div>
           <div class="text name">
-            {{nickname}}
+            {{ nickname }}
           </div>
           <div class="gutter"></div>
           <div class="text area">
             {{ getAreaName(player.area) }}
           </div>
           <div class="gutter"></div>
-          <div class="text level">
-            等级: {{ player.level }}
-          </div>
-        
+          <div class="text level">等级: {{ player.level }}</div>
         </div>
       </div>
 
-      <div class="empty" v-else>
-        没有结果
-      </div>
+      <div class="empty" v-else>没有结果</div>
     </panel>
   </div>
 </template>
 
 <script>
-import rq from "../utils/request"
-import panel from "../components/view/panel.vue"
-import icons from "../components/icons.vue"
-import areas from "../const/areas"
+import rq from "../utils/request";
+import panel from "../components/view/panel.vue";
+import icons from "../components/icons.vue";
+import areas from "../const/areas";
 
 export default {
-  components:{
+  components: {
     panel,
-    icons
+    icons,
   },
-  props:[
-    "nickname"
-  ],
-  data(){
+  props: ["nickname"],
+  data() {
     return {
-      ready:false,
-      errorText:"",
-      players:[]
-    }
+      ready: false,
+      errorText: "",
+      players: [],
+    };
   },
-  async mounted(){
-    await Promise.all([
-      this.search()
-    ])
+  async mounted() {
+    await Promise.all([this.search()]);
     this.ready = true;
   },
-  methods:{
-    async search(){
-      try{
-        let {players} = await rq.SearchPlayer({
-          "nickname": this.nickname,
-          "from_src": "lol_helper"
+  methods: {
+    async search() {
+      try {
+        let { players } = await rq.SearchPlayer({
+          nickname: this.nickname,
+          from_src: "lol_helper",
         });
         // 等级高的在前面
-        this.players = players.sort((a,b)=>b.level - a.level);
-      }catch(e){
+        this.players = players.sort((a, b) => b.level - a.level);
+      } catch (e) {
         this.errorText = e.message;
       }
     },
-    toPlayer({area, openid}){
-      this.$router.push({ name: 'summary', params: {
-        area: area,
-        accountType: 2,
-        id: openid
-      }});
+    toPlayer({ area, openid }) {
+      this.$router.push({
+        name: "summary",
+        params: {
+          area: area,
+          accountType: 2,
+          id: openid,
+        },
+      });
     },
-    getAreaName(id){
-      return areas.find(v=>v.v == id).t;
+    getAreaName(id) {
+      let v = areas.find((v) => v.v == id);
+      if (v) {
+        return v.t;
+      } else {
+        return "未知-" + id;
+      }
     },
-
-  }
-}
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .common{
-    display:flex;
-    flex-direction: column;
-    flex:auto;
-    padding: 40px 80px;
-    .players{
-      .player{
-        display:flex;
-        flex-direction: row;
-        height:40px;
-        padding:10px;
-        cursor:pointer;
-        &:hover{
-          background-color:rgba(255,255,255,0.1);
-        }
-        .gutter{
-          width:1px;
-          background-color: #24292e;
-          margin:0 15px;
-        }
-        .icon{
-          width:40px;
-          height:40px;
-          object-fit: contain;
-        }
-        .text{
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width:150px;
-        }
+.common {
+  display: flex;
+  flex-direction: column;
+  flex: auto;
+  padding: 40px 80px;
+  .players {
+    .player {
+      display: flex;
+      flex-direction: row;
+      height: 40px;
+      padding: 10px;
+      cursor: pointer;
+      &:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+      }
+      .gutter {
+        width: 1px;
+        background-color: #24292e;
+        margin: 0 15px;
+      }
+      .icon {
+        width: 40px;
+        height: 40px;
+        object-fit: contain;
+      }
+      .text {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 150px;
       }
     }
-    .errorText{
-      display:flex;
-      height:60px;
-      font-size: 14px;
-    }
-    .empty{
-      display:flex;
-      height:60px;
-      justify-content: center;
-      align-items: center;
-    }
   }
+  .errorText {
+    display: flex;
+    height: 60px;
+    font-size: 14px;
+  }
+  .empty {
+    display: flex;
+    height: 60px;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
