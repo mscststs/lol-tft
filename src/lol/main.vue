c53096f7f478fad572179561a7f7dc625371d13a
mosaicpan <mosaicpan@tencent.com>
2022-12-11 12:01:27 +0800

    update:bg

--- src/lol/main.vue
+++ src/lol/main.vue
@@ -3,8 +3,7 @@
     <tftHeader></tftHeader>
 
     <div class="wrap">
-      <div class="bgImg">
-      </div>
+      <div class="bgImg"></div>
       <div class="comp">
         <router-view :key="$route.path"></router-view>
       </div>
@@ -13,67 +12,65 @@
 </template>
 
 <script>
-
-import tftHeader from "./components/header.vue"
+import tftHeader from "./components/header.vue";
 export default {
-  components:{
+  components: {
     tftHeader,
   },
-}
+};
 </script>
 
-
 <style lang="less">
-  @import "./assets/common.less";
-  
-  *{
-    box-sizing: content-box;
-  }
-  .main{
-    display:flex;
-    flex:auto;
-    width:100%;
-    height:100%;
-    background-color:#19212b;
-    color:#ddd;
-    box-sizing: border-box;
-    flex-direction: column;
-  }
-  .wrap{
-    display:flex;
-    flex:auto;
-    position: relative;
-    overflow: hidden;
-    .bgImg{
-      position:absolute;
-      left:0;
-      top:0;
-      width:100%;
-      height:100%;
-      background-image: url(https://game.gtimg.cn/images/lol/lolstrategy/bg.jpg);
-      background-size: 100% auto;
-      background-repeat: no-repeat;
-      z-index:1;
-    }
-    .comp{
-      display:block;
-      width:100%;
-      z-index:10;
-      overflow:auto;
-      overflow:overlay;
-    }
-  }
+@import "./assets/common.less";
 
-  // 滚动条
-  ::-webkit-scrollbar{
-    width:8px;
-    height:8px;
+* {
+  box-sizing: content-box;
+}
+.main {
+  display: flex;
+  flex: auto;
+  width: 100%;
+  height: 100%;
+  background-color: #19212b;
+  color: #ddd;
+  box-sizing: border-box;
+  flex-direction: column;
+}
+.wrap {
+  display: flex;
+  flex: auto;
+  position: relative;
+  overflow: hidden;
+  .bgImg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url("./assets/bg.webp");
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+    z-index: 1;
   }
-  ::-webkit-scrollbar-thumb{
-    background:#b59758;
-    border-radius:5px;
+  .comp {
+    display: block;
+    width: 100%;
+    z-index: 10;
+    overflow: auto;
+    overflow: overlay;
   }
-  ::-webkit-scrollbar-track{
-    background:#19212b 0 0;
-  }
+}
+
+// 滚动条
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+::-webkit-scrollbar-thumb {
+  background: #b59758;
+  border-radius: 5px;
+}
+::-webkit-scrollbar-track {
+  background: #19212b 0 0;
+}
 </style>
